5a4f1ecf0b4298e99ba760ae26da837e3b62981f
Carlos <512.quadraphonic@gmail.com>
2015-02-07 17:52:49 -0500

    getMenu and QuickSort

--- top.py
+++ top.py
@@ -1,6 +1,7 @@
 #!venv/bin/env python
 
 import settings_local
+import calendar
 from item import Item, Menu, NutritionLabel
 from parse_rest.connection import register
 from datetime import datetime
@@ -35,6 +36,27 @@ def itemWithScore(item,nutritionLabel,freqTuple):
 
 	return (item,tumScore,yumScore)
 
-def getMenu(dateTime,location):
+def getMenu(timeStamp,location):
 	register(settings_local.APPLICATION_ID,settings_local.REST_API_KEY)
-	
+	date = "{} {},{}".format(calendar.month_abbr[timeStamp.month],timeStamp.day,timeStamp.year)
+	output_date = date
+	meal = 0
+
+	if(len(date) < 11):
+		output_date = date[:4] + '0' + date[4:]
+
+	time = (timeStamp.hour)*60 + timeStamp.minute
+
+	if (time >=420 && time < 660):
+		meal = 0
+	elif (time >= 660 && time < 1080):
+		meal = 1
+	elif (time >= 1080 && time < 1260):
+		meal = 2
+	else:
+		return 0
+
+
+
+	return Menu.Query.get(Date=output_date,Meal=meal,Location=location)
+
