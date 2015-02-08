a2ee1ac5850e33ca9333c2d788d7547cfde9980e
Anna <agonchar@bu.edu>
2015-02-08 10:19:24 -0500

    added comments

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,6 @@
 window.onload = function () {
-
-	var values;
-
 	
-	var frequencyChart1 = {
+	var frequencyChart1 = {  // HARDCODED CHART
 		title: {
 			text: "Frequency Analysis"
 		},       
@@ -20,7 +17,9 @@ window.onload = function () {
 				}
 			]
 	};
-	/*var frequencyChart = {
+	
+	/* #### TEST CHART
+	var frequencyChart = {
 		title: {
 			text: "Frequency Analysis"
 		},       
@@ -38,53 +37,42 @@ window.onload = function () {
 			]
 	};*/
 
-
-	console.log(frequencyChart1.data[0].dataPoints[2]);
-
-	$("td").click(function() {
-		var URL = "";
+	$("td").click(function() { // gets the click on the tables of people 
+		
+		/* #### ignore for now -- future query generator
+		var URL = ""; 
 	  	
 	  	var name = $(this).html();
-	  	var time = "2015-02-06 19:00:00"; 		// time and location are hardcoded in
-	  	var location = "Baystate";				// can later create input options
-	  
-		URL += encodeURI("ajax/name" + String(name)) //String(time) + "&location="+ String(location));
+	  	var time = "2015-02-06 19:00:00"; 		
+	  	var location = "Baystate";				
+		URL += encodeURI("ajax/name" + String(name)) //String(time) + "&location="+ String(location)); */
 
-	$.getJSON("/check", function(result){
-		 
-		console.log(result);  //{ Tikka Masala Sauce: 414.6712645435273}
-		//foo = $.parseJSON(result);
-		//console.log(foo);
-		i = 0;
-		
-		var dict ={};
-		
-		for (var name in result){ //dynamically attaches items
+		$.getJSON("/check", function(result){
+			 
+			console.log(result);  // returns in this format{Tikka Masala Sauce: 414.6712645435273}
 			
-			data = result[name];
+			i = 0;
 			
-			dict.y = data/10;
+			var dict ={}; // create new dictionary for formating it inside the chart
 			
-			dict.label = String(name);
+			for (var name in result){ //dynamically attaches items
+				
+				data = result[name];  // frequency value
+				
+				dict.y = data;  // appends freq. value to the dictionary's y value
+				
+				dict.label = String(name); // label gets changed to the food item name
+				
+				console.log("object", dict); // test whether the dictionary got created
+				
+				frequencyChart.data[0].dataPoints[i] = dict; // append the dictionary to the appropriate value
+				i++;
 			
-			console.log("i",i);
-			
-			console.log("object", dict);
-			
-			frequencyChart.data[0].dataPoints[i] = dict;
-			i++;
-		
-		};
-		//$("#frequency").CanvasJSChart(frequencyChart);
-		
-		console.log(frequencyChart.data[0].dataPoints);
-		
-		
+			}; // for loop ends
 
-	});
-
-	
-});
+		}); // json call ends
+		
+	}); // click ends
 
 $("#frequency1").CanvasJSChart(frequencyChart1);
 	
